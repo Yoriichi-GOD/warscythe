84296bd3727c65b88ca739d276e4b1ec4275a7a1
YoriichiGOD <nrgenosop@gmail.com>
2026-07-26 16:51:54 +0530

    sss

--- src/pages/Ledger.jsx
+++ src/pages/Ledger.jsx
@@ -391,7 +391,7 @@ export default function Ledger({ initialSubTab = 'history', onSubTabChange }) {
   };
 
   return (
-    <div className="w-full max-w-7xl mx-auto px-4 pt-6 pb-32 flex flex-col gap-6 relative">
+    <div className="ledger-page-shell w-full max-w-7xl mx-auto px-4 pt-6 pb-32 flex flex-col gap-6 relative">
       {/* Page Header */}
       <div className="flex flex-col sm:flex-row sm:items-center justify-between gap-4 border-b border-white/5 pb-4">
         <div className="flex flex-col gap-1">
